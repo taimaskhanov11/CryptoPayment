827ee5d9a01c69aedfe780daca868d666aa8a142
Rasul Taimaskhanov <81189712+taimaskhanov11@users.noreply.github.com>
2022-05-03 13:04:04 +0300

    initial

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ app = FastAPI()
 
 
 @app.get("/")
-async def root():
+async def root(request: Request):
+    print(request.url)
+    print(await request.body())
     return {"message": "Hello World"}
 
 
 @app.get("/success")
 async def success(request: Request):
+    print(request.url)
     print(await request.body())
     # print(await request.json())
 
