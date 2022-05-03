77b2568d0778a6382a52de83e942a5da208bbae4
Rasul Taimaskhanov <81189712+taimaskhanov11@users.noreply.github.com>
2022-05-03 16:55:53 +0300

    initial

--- main.py
+++ main.py
@@ -1,36 +1,27 @@
 from fastapi import FastAPI, Request
+from starlette.middleware.trustedhost import TrustedHostMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    TrustedHostMiddleware, allowed_hosts=["193.232.179.110"]
+)
 
 
 @app.get("/")
 async def root(request: Request):
     print(request.url)
     print(await request.body())
+    print(request.client)
     return {"message": "Hello World"}
-
-
-@app.get("/success")
-async def success(request: Request):
-    print(request.url)
-    print(await request.body())
-    # print(await request.json())
-
-
-@app.get("/failure")
-async def failure(request: Request):
-    print(await request.body())
-    # print(await request.json())
-
 
 @app.get("/dispatcher")
 async def dispatcher(request: Request):
+    print(request.client)
     print(await request.body())
-    # print(await request.json())
+
 
 @app.post("/dispatcher")
 async def dispatcher(request: Request):
+    print(request.client)
     print(await request.body())
-    # print(await request.json())
-
-
